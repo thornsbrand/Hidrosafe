d55c3bb8099f196d15f60ce59f115d45c723a6bd
thornsbrand <cristiancamiloortiz48@gmail.com>
2025-02-13 12:19:03 -0500

    Descripción de los cambios

--- auth.py
+++ auth.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required
 from urllib.parse import urlparse
 from app import db
-from models import User
 
 auth_bp = Blueprint('auth', __name__)
 

669d7cd8b18b4c6414cf1833c59136691ea749d0
Robinson Segura Aponte <56366357+robinsons1@users.noreply.github.com>
2025-02-16 16:21:06 -0500

    Descripción de los cambios

--- routes.py
+++ routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, request, jsonify
 from models import add_system_data, get_all_system_data
 from firebase_admin import firestore, auth
-from auth import auth_bp  # Asegura que esta línea está presente
+from auth import auth_bp
+from flask_login import login_required
 
 # Inicializar Firestore
 db = firestore.client()
@@ -15,10 +16,10 @@ def index():
     return render_template('home.html')
 
 @main.route('/dashboard')
+@login_required
 def dashboard():
-    recent_data = get_all_system_data()
-    return render_template('dashboard.html', data=recent_data)
-
+    return render_template('dashboard.html')
+    
 @main.route('/documentation')
 def documentation():
     return render_template('documentation.html')

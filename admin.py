a2ccaa3fdb8315934dc376bb65e0d2708c235a98
Robinson Segura Aponte <56366357+robinsons1@users.noreply.github.com>
2025-02-16 21:05:14 -0500

    Descripción de los cambios

--- admin.py
+++ admin.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template, abort
 from flask_login import login_required, current_user
+from firebase_admin import firestore
 
 admin_bp = Blueprint('admin', __name__, url_prefix='/admin')
+db = firestore.client()
 
 @admin_bp.route('/')
 @login_required
@@ -9,3 +11,11 @@ def admin_panel():
     if current_user.rol != "admin":
         abort(403)  # 🔹 Acceso prohibido para usuarios normales
     return render_template('admin_panel.html')
+
+@admin_bp.route('/manage_users')
+@login_required
+def manage_users():
+    if current_user.rol != "admin":
+        abort(403)
+    users = db.collection("usuarios").get()
+    return render_template('admin_manage_users.html', users=users)
